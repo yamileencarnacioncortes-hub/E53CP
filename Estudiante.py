1eb891508ab5862f07322e63af49e225ac3c3481
yamileencarnacioncortes-hub <yamileencarnacioncortes@gmail.com>
2025-12-10 17:20:21 -0600

    Add files via upload
    aquí meramos el código para ver como se iba a dar de alta el alumno para que pasara a la parte del test

---
+++ Estudiante.py
@@ -0,0 +1,6 @@
+class Estudiante:
+    def __init__(self,nombre,edad,correo,telefono):
+        self.nombre = nombre
+        self.edad = edad
+        self.correo = correo
+        self.telefono = telefono                        
