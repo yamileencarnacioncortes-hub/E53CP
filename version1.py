62f10dfbc8271cbbb7c5d3c4da1dda2966675b9f
yamileencarnacioncortes-hub <yamileencarnacioncortes@gmail.com>
2025-12-10 12:46:43 -0600

    Add files via upload
            hicimos nuestra primera ventana su función era poner el color que quisieramos que fue color verde menta

---
+++ version1.py
@@ -0,0 +1,10 @@
+#importar las bibliotecas
+import tkinter as tk
+#configurar la ventana 
+ventana = tk.Tk()
+ventana.title("E53CP TEST DE ADICCIONES A LAS REDES SOCIALES") # agregamos titulo a la ventana
+ventana.geometry("600x400") #agregamos medidas a la ventana
+ventana.config(bg="#6CE49E") # agregamos color a la ventana
+
+# MANDAR A TRAER MI VENTANA
+ventana.mainloop()
